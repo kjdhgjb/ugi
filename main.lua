b7bae15a742194e09482ffc84e0cfd8b69e71e78
kjdhgjb <lamsalaaditya19@gmail.com>
2026-01-19 17:10:35 +0545

    main.lua
    Removed the Infinite Jump button and its associated functionality.

--- main.lua
+++ main.lua
@@ -46,35 +46,4 @@ Rayfield:Notify({
 
 local Tab = Window:CreateTab("Home tab ", 4483362458) -- Title, Image
 
- local Button = MainTab:CreateButton({
-   Name = "Infinite Jump",
-   Callback = function()
-           --Toggles the infinite jump between on or off on every script run
-_G.infinjump = not _G.infinjump
 
-if _G.infinJumpStarted == nil then
-	--Ensures this only runs once to save resources
-	_G.infinJumpStarted = true
-	
-	--Notifies readiness
-	game.StarterGui:SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
-
-	--The actual infinite jump
-	local plr = game:GetService('Players').LocalPlayer
-	local m = plr:GetMouse()
-	m.KeyDown:connect(function(k)
-		if _G.infinjump then
-			if k:byte() == 32 then
-			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
-			humanoid:ChangeState('Jumping')
-			wait()
-			humanoid:ChangeState('Seated')
-			end
-		end
-	end)
-end
-   end,
-})
-
-   end,
-})
